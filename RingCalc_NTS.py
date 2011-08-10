3467d428db5510051b1c048bf46177a0a51f5a24
Buddy Chell <rnchell@gmail.com>
2011-08-10 10:33:09 -0300

    changed arg parsing methods

--- RingCalc_NTS.py
+++ RingCalc_NTS.py
@@ -15,59 +15,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-import sys,getopt,platform
+import argparse
 
 rings = {}
 useNTS = False
 nodes = 0
 datacenters = 0
 output = 0
+parser = None
+
+def isEntry(f):
+	if __name__ == "__main__":
+		f()
+	return f
 
 def hash_line():
 	print("#"*60)
 
 def is_int(s):
 	try: 
-	  int(s)
+	  int(str(s))
 	  return True
 	except ValueError:
 	  return False
-	
-def usage():
-	print(
-'''--nts
-  If set, generates multiple overlapping rings for
-	NetworkTopologyStrategy.
-  If not set, makes a single ring.
-
--n or --nodes
-  Sets the number of nodes on the ring.  If --nts is set it sets the
-  number of nodes at each datacenter.
-
--d or --datacenters
-  Required for --nts.  Sets the number of datacenters to build
-  overlapping rings for.
-
--o or --output
-  For NTS, output a specific datacenter.
-	Example:
-  --------
-
-  # A single ring with 10 nodes.
-  ./ringCalc.py -n 10
-
-  # 2 overlapping NTS style rings with four nodes.
-  ./ringCalc.py --nts -n 4 -d 2 
-
-  # 3 overlapping NTS style rings with 8 nodes, show only datacenter 3.
-  ./ringCalc.py --nts -n 8 -d 3 -o 3'''
-)
-	sys.exit()
-	
+	  
 def error(message):
 	print(">>>>ERROR: %s" % message)
 	
+def usage():
+	parser.print_help()
+
 def nts_tokens():
 	initialTokens = list(map(lambda y: "%d" % ((2**127)*y/nodes), range(0,nodes)))
 	if output > 0:
@@ -76,9 +53,7 @@ def nts_tokens():
 			hash_line()
 			print("Data Center Ring", output)
 			hash_line()
-			for n in range(nodes):
-				print("Node:", n)
-				print("\tinitial_token:", tokens[n])
+			single_ring_tokens(tokens)
 		else:
 			error("output arg is greater than number of datacenters")
 			usage()
@@ -87,60 +62,54 @@ def nts_tokens():
 		for d in range(datacenters):
 			rings[d] = list(map(lambda t: int(t)+increment, initialTokens))
 			increment += 1
-			ringCount = 0
 			hash_line()
 			print("Data Center Ring", d+1)
 			hash_line()
-			for v in rings[d]:
-				print("Node", str(ringCount) + ":")
-				print("\tinitial_token"+":",v)
-				ringCount += 1
+			single_ring_tokens(rings[d])
 				
-def single_ring_tokens():
-	tokens = list(map(lambda y: "%d" % ((2**127)*y/nodes), range(0,nodes)))
+def single_ring_tokens(_tokens=None):
+	if _tokens is None:
+		tokens = list(map(lambda y: "%d" % ((2**127)*y/nodes), range(0,nodes)))
+	else:
+		tokens = _tokens
 	for n in range(nodes):
 		print("Node:", n)
 		print("\tinitial_token:", tokens[n])
-		
-def main(argv):
+
+def parse_args():
 	global useNTS,nodes,datacenters,output
-	if not argv:
-		usage()
-	try:
-		opts, args = getopt.getopt(argv, "d:o:n:", ["help","nts","nodes=","datacenters=","output="])
-	except getopt.GetoptError as e:
-		error(e)
-		usage()
-	for opt, arg in opts:
-		if opt == "--nts":
-			useNTS = True
-		elif opt == "--help":
-			usage()
-		elif opt in ["-n","--nodes"]:
-			if is_int(arg):
-				nodes = int(arg)
-			else:
-				usage()
-		elif opt in ["-d","--datacenters"]:
-			if is_int(arg):
-				datacenters = int(arg)
-			else:
-				usage()
-		elif opt in ["-o","--output"]:
-			if is_int(arg):
-				output = int(arg)
-			else:
-				usage()
+	args = parser.parse_args()
+	useNTS = args.nts
+	if is_int(args.nodes):
+		nodes = int(args.nodes)
+	if is_int(args.datacenters):
+		datacenters = int(args.datacenters)
+	if is_int(args.output):
+		output = int(args.output)
+
+# initialize script args
+def args_init():
+	global parser
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-o', '--output',help='For NTS, output a specific datacenter.')
+	parser.add_argument('-n', '--nodes',help='Sets the number of nodes on the ring.  If --nts is set it sets the number of nodes at each datacenter.')
+	parser.add_argument('-d','--datacenters',help='Required for --nts.  Sets the number of datacenters to build overlapping rings for.')
+	parser.add_argument('--nts', help='If set, generates multiple overlapping rings for NetworkTopologyStrategy. If not set, makes a single ring.', action='store_true')
+	
+@isEntry
+def main():
+	global useNTS,nodes,datacenters
+	
+	args_init()
+	parse_args()
+	
 	#if nts flag is set, nodes and datacenters must be > 0
 	if useNTS and nodes > 0 and datacenters > 0:
 		nts_tokens()
+		
 	#if nts is not set, nodes flag must be > 0 and datacenters must be 0
 	elif not useNTS and nodes > 0 and datacenters == 0:
 		single_ring_tokens()
 	else:
 		error("not all required flags set")
 		usage()
-		
-		
-if __name__ == "__main__":
-    main(sys.argv[1:])
